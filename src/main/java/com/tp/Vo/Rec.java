6be9abb858bbc8f447c82a690ffde3e4da9c1019
user <user@222.112.31.36>
2023-02-14 08:31:22 +0900

    123

--- src/main/java/com/tp/Vo/Rec.java
+++ src/main/java/com/tp/Vo/Rec.java
@@ -4,7 +4,7 @@ public class Rec
 {
 	private int rnum;  		// 시퀀스 레시피 번호
 	private String title;	// 레시피 제목
-	private String gastr;	// 요리법
+	private String info;	// 요리소개
 	private String author;	// 작성자
 	private int hits;		// 조회수
 	private java.sql.Date recdate;	// 작성일
