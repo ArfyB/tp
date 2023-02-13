c86ce72d9cc8acc2690823074d1c2ee16536427a
Oh Sang Jin <arfybundle@gmail.com>
2023-02-13 17:49:02 +0900

    aaaaaaaaaaaaaaaaaa

---
+++ src/main/java/com/tp/Vo/Rec.java
@@ -0,0 +1,12 @@
+package com.tp.Vo;
+
+public class Rec 
+{
+	private int rnum;  		// 시퀀스 레시피 번호
+	private String title;	// 레시피 제목
+	private String gastr;	// 요리법
+	private String author;	// 작성자
+	private int hits;		// 조회수
+	private java.sql.Date recdate;	// 작성일
+	
+}
