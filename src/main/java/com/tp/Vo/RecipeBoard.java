2e5701fe2705aaf6a1202e887eea0c36ec983985
user <user@220.70.26.195>
2023-02-23 06:04:14 +0900

    1111111111111111

---
+++ src/main/java/com/tp/Vo/RecipeBoard.java
@@ -0,0 +1,23 @@
+package com.tp.Vo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Component
+@Data //get set
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecipeBoard 
+{
+	private int bnum;  		// 시퀀스 레시피 번호	시퀀스 
+	private String title;	// 레시피 제목			@
+	private String author;	// 작성자				
+	private int hits;		// 조회수
+	private String contents;
+	private java.sql.Date recdate;	// 작성일		
+}
