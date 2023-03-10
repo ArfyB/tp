1ce3c5aa26362e023283462d5d6c9c96ca9f6a48
shamp54 <121779109+shamp54@users.noreply.github.com>
2023-03-10 16:56:10 +0900

    41121 21

--- src/main/java/com/tp/Vo/Recipe.java
+++ src/main/java/com/tp/Vo/Recipe.java
@@ -21,8 +21,8 @@ public class Recipe
 	private int hits;		// 조회수				
 	private java.sql.Date recdate;	// 작성일		
 	private String category;	// 가장 큰 카테고리	@
-	private String how;			// 방법			@ 11
-	private String catemat;		// 주 재료		@ 11
+	private String how;			// 방법			@
+	private String catemat;		// 주 재료		@
 	private String human;		// 몇인분			@
 	private int time;		// 조리시간			@
 	private String difficult;	// 난이도			@
